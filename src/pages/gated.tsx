f9a7033b3a7a7c7b50e8fe5f707381f587d1830c
Angela Gilhotra <angelagilhotra@gmail.com>
2023-08-30 11:26:48 -0700

    trigger deploy

--- src/pages/gated.tsx
+++ src/pages/gated.tsx
@@ -13,7 +13,7 @@ export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
     return {
       redirect: {
         permanent: false,
-        destination: '/login', // Redirect if wallet does not have the required token
+        destination: '/login',
       },
     };
   }
