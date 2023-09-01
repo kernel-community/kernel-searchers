5a30705e7385bf0c63f952433ef8d830b188233a
Angela Gilhotra <angelagilhotra@gmail.com>
2023-09-01 02:55:52 -0700

    add how page

---
+++ src/pages/how.tsx
@@ -0,0 +1,19 @@
+import Main from "src/layout/Main";
+
+export default function How() {
+  return (
+    <Main>
+      <h1 className="tracking-tight text-[3rem]">
+        How does it work
+      </h1>
+      <article className="prose">
+        <p>
+          View the manual&nbsp;
+          <a href="https://www.notion.so/Kernel-Searching-Manual-76500dea72c54eb1b3e23e0ddbe79533?pvs=4" target="_blank">
+            here
+          </a>
+        </p>
+      </article>
+    </Main>
+  )
+}
--- src/server/utils/constants.ts
+++ src/server/utils/constants.ts
@@ -2,5 +2,5 @@ export const DEV_URL = "http://localhost:3001";
 export const PROD_URL = "https://kernel-searchers.vercel.app";
 
 // yyyy-mm-dd
-export const SEARCHING_START_DATE = "2023-08-07";
+export const SEARCHING_START_DATE = "2023-08-06";
 
