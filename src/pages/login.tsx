972c70e78d752de502adff14893a354e3fb543bb
Angela Gilhotra <angelagilhotra@gmail.com>
2023-09-15 05:07:15 -0700

    add warning message for signature

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -2,7 +2,10 @@ import type {
   // GetServerSideProps,
   NextPage
 } from "next";
+import Link from "next/link";
 import { RetroConnectKitButton } from "src/components/RetroButton";
+import { useIsSearcher } from "src/hooks/useIsSearcher";
+import { useAccount } from "wagmi";
 // import { siweServer } from "src/server/utils/siweServer";
 // import walletIsSearcher from "src/server/utils/walletIsSearcher";
 
@@ -23,17 +26,38 @@ import { RetroConnectKitButton } from "src/components/RetroButton";
 //   };
 // };
 
-const CollectorsOnlyPage: NextPage = () => {
+const LoginPage: NextPage = () => {
+  const {isSearcher} = useIsSearcher();
+  const {address} =  useAccount();
   return (
-    <>
-      <div className="font-playfair text-4xl py-4 pl-4 flex flex-row gap-3 items-center">
+    <div className="p-6 flex flex-col gap-12">
+      <div className="font-playfair text-4xl flex flex-row gap-3 items-center">
         Login to continue with the Searcher Portal
         <span>
           <RetroConnectKitButton />
         </span>
       </div>
-    </>
+
+      {
+          address && isSearcher ?
+          <div className="flex flex-col gap-4">
+              <div className="alert alert-warning w-fit">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-info shrink-0 w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+              <span>Make sure you have signed on the message. Without doing so the following link will not redirect you.</span>
+            </div>
+          <Link className="font-miriam cursor-pointer text-3xl" href={"/"}>
+                Click
+                <span className="text-primary">
+                &nbsp;here&nbsp;
+                </span>
+                to start searching
+              </Link>
+          </div>
+          :
+            <></>
+        }
+    </div>
   );
 };
 
-export default CollectorsOnlyPage;
+export default LoginPage;
--- src/utils/onChainConfig.ts
+++ src/utils/onChainConfig.ts
@@ -1,4 +1,3 @@
-import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
 import { alchemyProvider } from "wagmi/providers/alchemy";
 import { configureChains } from "wagmi";
 import { goerli } from "wagmi/chains";
@@ -9,11 +8,11 @@ export const { publicClient, webSocketPublicClient } = configureChains(
   CHAINS,
   // /env.mjs ensures the the app isn't built without .env vars
   [
-    jsonRpcProvider({
-      rpc: () => ({
-        http: "http://localhost:8545",
-      })
-    }),
+    // jsonRpcProvider({
+    //   rpc: () => ({
+    //     http: "http://localhost:8545",
+    //   })
+    // }),
     alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_ID! })
   ],
 )
