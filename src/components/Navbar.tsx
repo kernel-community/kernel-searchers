8f1e1002501ef2cbe7015bf0399472a6d1cc0505
Angela Gilhotra <angelagilhotra@gmail.com>
2024-01-10 18:39:42 -0800

    remove title from navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,17 +5,15 @@ const Branding = () => {
   return (
     <div className="tracking-tight cursor-pointer font-futura">
       <Link href={"/"}>
-        Kernel
+        Kernel Atlas
       </Link>
     </div>
   )
 }
 export default function Navbar () {
-  const title = "Kernel Atlas"
   return (
     <div className="navbar flex flex-row justify-between border-2 border-primary-content">
       <Branding />
-      <div className="hidden md:block">{title}</div>
       <div className="md:flex-row md:gap-2 items-center hidden md:flex">
         <DynamicLoginButton />
       </div>
@@ -28,7 +26,6 @@ export default function Navbar () {
           <label htmlFor="my-drawer" className="drawer-overlay"></label>
           <div className="menu w-80 p-4 min-h-full bg-base-200 text-base-content">
             <div className="flex flex-col gap-6 [&>*]:bg-base-100 [&>*]:p-2 [&>*]:rounded-md">
-              <div className="">{title}</div>
               <div className="flex flex-col gap-2">
               <DynamicLoginButton />
               </div>
